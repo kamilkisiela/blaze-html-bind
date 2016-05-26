85fc5eaab9dbfb8a4b7f10d8462a3518aa3f2335
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-05-26 21:05:41 +0200

    raw escaped manual

--- client/main.js
+++ client/main.js
@@ -2,12 +2,11 @@ import { Template } from 'meteor/templating';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  this.specialChars = '&gt;';
-});
-
 Template.hello.helpers({
-  specialChars() {
-    return Template.instance().specialChars;
+  raw() {
+    return '>';
+  },
+  escaped() {
+    return '&gt;';
   }
 });
